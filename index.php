6087e07076ede031e319d38a47f56ec5c602afd1
pydev <xtfkpi@gmail.com>
2016-08-01 15:09:52 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo 'Hello world from Cloud9!';
+echo 'Текущая версия PHP: ' . phpversion();
+
+?>
+</body>
+</html>
+</body>
+</html>
